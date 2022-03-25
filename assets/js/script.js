5f2336e5bec899230ad029bded906b2c6c9b8db6
Heather Noyes <heatherknoyes@gmail.com>
2022-03-24 22:25:30 -0400

    Added example questions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,50 @@
 var highscoreEl = document.querySelector(".highscore-button");
+var startContentEl = document.querySelector(".start-content");
 var startEl = document.querySelector(".start-button");
 var timerEl = document.querySelector(".timer");
 
+var questions = [
+  {
+    question: "What punctuation ends a method in Javascript?",
+    answers: ["comma", "apostrophe", "parentheses", "period"],
+    answer: 0,
+  },
+  {
+    question: "What keyword defines a variable in Javascript?",
+    answers: ["const", "var", "let", "All of the above"],
+    answer: 0,
+  },
+  {
+    question:
+      "How would you present a popup message to the user in Javascript?",
+    answers: [
+      "window.alert()",
+      "console.log()",
+      "document.open()",
+      "window.popup()",
+    ],
+    answer: 0,
+  },
+  {
+    question: "What are three main front end languages?",
+    answers: [
+      "Javascript, HTML, CSS",
+      "Markdown, Javascript, CSS",
+      "HTML, CSS, Java",
+      "HTML, Java, Angular",
+    ],
+    answer: 0,
+  },
+  {
+    question:
+      "Which function is used to serialize an object into a JSON string in Javascript?",
+    answers: ["stringify()", "parse()", "convert()", "All of the above"],
+    answer: 0,
+  },
+];
+
 startEl.addEventListener("click", function () {
-  // createBlankWord(word);
+  startContentEl.style.display = "none";
   timer();
 });
 
